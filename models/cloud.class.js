c63c0191e3eb7d76e32dac6edc257c47ae1ac3d8
BastianThoma <ossi_1993@hotmail.de>
2024-06-13 13:48:02 +0200

    created constructor on cloud class and changed title

---
+++ models/cloud.class.js
@@ -0,0 +1,10 @@
+class Cloud extends MovableObject {
+    
+    constructor() {
+        super().loadImage('../img/5_background/layers/4_clouds/1.png');
+
+        this.x = 200 + Math.random() * 400;
+    }
+
+
+}
