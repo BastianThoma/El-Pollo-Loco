fd9cc7381c9e062c3eab0fea43858d6a05864d3c
BastianThoma <ossi_1993@hotmail.de>
2024-06-13 13:44:23 +0200

    changed imgs and changed headline

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,6 +1,6 @@
 class Chicken extends MovableObject {
     constructor() {
-        super().loadImage('../img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
+        super().loadImage('../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
 
         this.x = 200 + Math.random() * 400;
     }
