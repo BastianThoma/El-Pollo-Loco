b3a8cdd2ea89727f049e3da827e8d4b077f62306
BastianThoma <ossi_1993@hotmail.de>
2024-06-18 17:45:17 +0200

    added new class and optimized the project structure

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,4 @@
-class MovableObject {
-    x = 120;
-    y = 290;
-    img;
-    height = 150;
-    width = 120;
-    imageCache = [];
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -24,17 +17,9 @@ class MovableObject {
     isAboveGround() {
         return this.y < 180;
     }
+
     isOnGround() {
         return this.y > 180;
-    }
-
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
     drawFrame(ctx) {
@@ -58,7 +43,7 @@ class MovableObject {
         this.energy -= 5;
         if (this.energy < 0) {
             this.energy = 0;
-        }else {
+        } else {
             this.lastHit = new Date().getTime();
         }
     }
@@ -71,14 +56,6 @@ class MovableObject {
 
     isDead() {
         return this.energy == 0;
-    }
-
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
     }
 
     playAnimation(images) {
